4d13ebbdf8a7f8f26a0109390a37c0ca5a825998
MuhammadSufiyan511 <108684203+MuhammadSufiyan511@users.noreply.github.com>
2025-07-29 14:17:43 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './Components/navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
